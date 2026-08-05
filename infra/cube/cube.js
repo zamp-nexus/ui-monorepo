fa6c7ab96b8ddb7e7c64c9e9abf53045b513b706
GowthamTG <gowthamgopinath1@gmail.com>
2026-08-05 09:53:37 +0530

    fix(cube): enable TLS for Neon warehouse

--- infra/cube/cube.js
+++ infra/cube/cube.js
@@ -72,6 +72,7 @@ module.exports = {
         port: process.env.CUBEJS_DB_PORT,
         user: process.env.CUBEJS_DB_USER,
         password: process.env.CUBEJS_DB_PASS,
+        ssl: process.env.CUBEJS_DB_SSL === 'true',
       };
     }
     if (!securityContext || !securityContext.dataConnectionId) {
